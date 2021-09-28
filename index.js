644531c9a0046808bd9aebf4ada3f60def0646de
Matt Eva <matteweva@gmail.com>
2021-09-28 14:56:03 -0700

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+const employee = {
+    name: `Bryan`,
+    address: `Hummdinger Lane`
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {...obj, [key]: value,};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function dstrctvlyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj.key = value;
+    return obj;
+}
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'name', `Dave`);
+console.log(employee);
+
+dstrctvlyUpdateEmployeeWithKeyAndValue(employee, 'name', `Jack`);
+console.log(employee);
+
+employee.name = 'Kevin';
+console.log(employee);
+
+employee['name'] = 'Jamal';
+
+function deleteFromEmployeeByKey(obj, key) {
+    return {...obj.key};
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
+
+destructivelyDeleteFromEmployeeByKey(employee, `name`);
+console.log(employee);
